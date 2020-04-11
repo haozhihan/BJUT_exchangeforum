de87310b4cd229f6f82539b17131fcda6d3ca5d4
jason_pro <535073436_pro@qq.com>
2020-04-11 11:24:41 +0800

    this is profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,15 @@
 from flask import render_template
 from . import main
+from ..models import User
 # view functions for index page
 # unfinished
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/user/<username>')
+def user():
+    users = User.query.filter_by(username=username).first_or_404
+    return render_template('user.html', user=users)
