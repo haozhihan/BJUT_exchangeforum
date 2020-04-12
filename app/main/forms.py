6818f8db5bced96e50d5228cb775a0a932d285cd
jason_pro <535073436_pro@qq.com>
2020-04-12 12:47:10 +0800

    this is profile

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length
 # unfinished
 # forms for index page
 
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('username', validators=[Length(0, 64)])
+    college = StringField('college', validators=[Length(0, 10)])
+    grade = StringField('grade', validators=[Length(0, 4)])
+    about_me = TextAreaField('About me')
     submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,11 @@
-from flask import render_template
+from flask import render_template, request, flash, redirect, url_for
 from . import main
-from ..models import User
+from ..models import User, db, Role
+from flask_login import login_required, current_user
+from .forms import EditProfileForm
 # view functions for index page
 # unfinished
+
 
 @main.route('/')
 def index():
@@ -10,6 +13,26 @@ def index():
 
 
 @main.route('/user/<username>')
-def user():
+def user(username):
     users = User.query.filter_by(username=username).first_or_404
     return render_template('user.html', user=users)
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.college = form.college.data
+        current_user.grade = form.grade.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user._get_current_object())
+        db.session.commit()
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.username.data = current_user.username
+    form.college.data = current_user.college
+    form.grade.data = current_user.grade
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
